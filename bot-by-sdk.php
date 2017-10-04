a050c60ce2769e2f04555524c6c214d1f970fbda
Watcharamet <watcharamet.sinethirothai@codemate.com>
2017-10-04 17:05:28 +0700

    fixbug

--- bot-by-sdk.php
+++ bot-by-sdk.php
@@ -29,34 +29,15 @@ $events = json_decode($content, true);
 
 if (!is_null($events['events'])) {
 
-    $replyToken = $event['replyToken'];
+    $replyToken = $events['replyToken'];
     
     $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('This message from bot');
     
     $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+
     if ($response->isSucceeded()) {
         return;
-    } else {
-            // Post message back to Line Server.
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$response->getRawBody()],
-			];
-            $post = json_encode($data);
-
-            // AccessToken use here. 
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-    }
+    } 
     
     // Failed
     //echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
