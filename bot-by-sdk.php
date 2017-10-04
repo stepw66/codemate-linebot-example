333be103b8b290ec4ce3fa22ec9ce5cab083cfa6
Watcharamet <watcharamet.sinethirothai@codemate.com>
2017-10-04 17:01:12 +0700

    add debug part.

--- bot-by-sdk.php
+++ bot-by-sdk.php
@@ -1,8 +1,11 @@
 <?php
 require_once('./vendor/autoload.php');
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('KbQAojIxyVyVscXVmUhvrvvwhWdsxsT6lKcmGDaORsN0iiXthesiJx2dBTRx05DCYz6LiXscNEs0SrUsyuxkS5u0TaH5CuUWl8qf9MZp914Dh2caSIMkP7nLDk8bE7paFcK5C1N05RA1LerVg47uOAdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '30bbe3ecdfd9aa90dbc95dcafc62ef41']);
+$access_token = 'KbQAojIxyVyVscXVmUhvrvvwhWdsxsT6lKcmGDaORsN0iiXthesiJx2dBTRx05DCYz6LiXscNEs0SrUsyuxkS5u0TaH5CuUWl8qf9MZp914Dh2caSIMkP7nLDk8bE7paFcK5C1N05RA1LerVg47uOAdB04t89/1O/w1cDnyilFU=';
+$secret_token = '30bbe3ecdfd9aa90dbc95dcafc62ef41';
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $secret_token]);
 
 /* Line text message structure.
  {
@@ -33,6 +36,26 @@ if (!is_null($events['events'])) {
     $response = $bot->replyMessage($replyToken, $textMessageBuilder);
     if ($response->isSucceeded()) {
         return;
+    } else {
+            // Post message back to Line Server.
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$response->getRawBody()],
+			];
+            $post = json_encode($data);
+
+            // AccessToken use here. 
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
     }
     
     // Failed
